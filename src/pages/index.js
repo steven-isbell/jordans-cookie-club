d4e2a565b8a6033feaaf36509a290cfcc57a171d
Steven Isbell <steveni_92@msn.com>
2018-08-21 14:44:38 -0500

    gatsby setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import Checkout from '../components/checkout'
+
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Checkout />
   </div>
 )
 
