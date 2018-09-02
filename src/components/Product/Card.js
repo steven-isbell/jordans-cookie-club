8a08fe83c45fc98717ccd97695993cc12191c1ea
Steven Isbell <steveni_92@msn.com>
2018-09-01 20:07:42 -0500

    add struct

---
+++ src/components/Product/Card.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Card = props => {
+  return <div />;
+};
+
+Card.propTypes = {};
+
+export default Card;
---
+++ src/components/Product/Container.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Container extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+Container.propTypes = {};
+
+export default Container;
