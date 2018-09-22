1dfaf7f7d0e53b4e09ed552057c2fe365fd99a8c
Steven Isbell <steveni_92@msn.com>
2018-09-21 22:16:40 -0500

    added sign up and sign in pages

---
+++ src/pages/sign_in.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const sign_in = () => {
+  return <div>Sign In!</div>;
+};
+
+export default sign_in;
---
+++ src/pages/sign_up.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const sign_up = () => {
+  return <div>Sign Up!</div>;
+};
+
+export default sign_up;
