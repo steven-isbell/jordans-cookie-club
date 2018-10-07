0afe749d5130528590e15fd94c55780221c3a3b3
Steven Isbell <steveni_92@msn.com>
2018-10-07 14:30:22 -0500

    adding cart

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,11 +1,4 @@
 import React from 'react';
-import Checkout from '../components/Checkout';
-
-const cookies = [
-  { id: 1, name: 'Boo Set of 4', price: 15 },
-  { id: 2, name: 'Halloween Mickey Set of 3', price: 15 },
-  { id: 3, name: 'Hello Fall Set of 3', price: 15 },
-];
 
 const cart = () => {
   return <div>Cart</div>;
