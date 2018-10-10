af192692b113e717da446bc9013d1b736fd58c28
Steven Isbell <steveni_92@msn.com>
2018-10-09 22:50:35 -0500

    adding serverless

--- server/formHandler.js
+++ server/formHandler.js
@@ -1,1 +1,16 @@
-module.exports.handler = (event, context, callback) => {};
+const sgMail = require('@sendgrid/mail');
+
+const { SENDGRID_KEY, CONTACT_EMAIL } = process.env;
+
+sgMail.setApiKey(SENDGRID_KEY);
+
+module.exports.handler = (event, context, callback) => {
+  const { name, email, description, address } = event;
+  const config = {
+    from: `${name} <${email}>`,
+    to: CONTACT_EMAIL,
+    subject: 'New Cookie Order',
+    text: `Message: ${message}, Desc: ${description}, Address: ${address}`,
+  };
+  sgMail.send(config);
+};
