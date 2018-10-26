57c2a63fe90f5731a5d1a387bb08d1ba58e0f132
Steven Isbell <steveni_92@msn.com>
2018-10-25 23:53:11 -0500

    updating name

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: "Jordan's Cookie Club",
+    title: "Clementine Cookie Co.",
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const HeaderContainer = styled.header`
   align-items: center;
   justify-content: space-between;
   margin-bottom: 1.45rem;
-  font-family: Lato, Helvetica Neue, Arial, Helvetica, sans-serif;
+  font-family: Clicker Script, Helvetica Neue, Arial, Helvetica, sans-serif;
   background: #fff;
   font-weight: 400;
   border: 1px solid rgba(34, 36, 38, 0.15);
@@ -43,7 +43,7 @@ class Header extends Component {
 }
 
 Header.defaultProps = {
-  title: "Jordan's Cookie Club",
+  title: 'Clementine Cookie Co.',
 };
 
 Header.propTypes = {
